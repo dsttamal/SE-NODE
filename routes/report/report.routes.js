a1c819ed74fc79ae0d869141d1bc365a6b59fb8f
dsttamal <dsttamal@gmail.com>
2023-06-09 19:04:21 +0600

    ""

--- routes/report/report.routes.js
+++ routes/report/report.routes.js
@@ -6,6 +6,6 @@ const reportRoutes = express.Router();
 reportRoutes.post("/add", authorize(), addReport);
 reportRoutes.get("/view", authorize(), viewAll);
 reportRoutes.get("/view/user/:id", authorize(), viewByUser);
-reportRoutes.get("/view/:id", authorize(), view);
+reportRoutes.get("/view/:id", view);
 
 module.exports = reportRoutes;
